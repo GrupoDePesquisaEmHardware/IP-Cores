2232801b68319678a98e2c3ce9305093be294dd2
Otávio Augusto <35488608+otavioacb@users.noreply.github.com>
2022-10-19 10:27:11 -0300

    Update display_7segmentos.vhd

--- Display de 7 Segmentos/display_7segmentos.vhd
+++ Display de 7 Segmentos/display_7segmentos.vhd
@@ -17,58 +17,58 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY display_7segmentos IS
 PORT( 
-		A_di7s, B_di7s, C_di7s, D_di7s: IN std_logic;
-		L1, L2, L3, L4, L5, L6, L7: OUT std_logic
+		A_di7s, B_di7s, C_di7s, D_di7s: IN STD_LOGIC;
+		L1, L2, L3, L4, L5, L6, L7: OUT STD_LOGIC
 	);
 END display_7segmentos;
 
 
 ARCHITECTURE logic OF display_7segmentos IS
-SIGNAL P_AND1, P_AND2, P_AND3, P_AND4, P_AND5, P_AND6, P_AND7, P_AND8, 
-		 P_AND9, P_AND10, P_AND11, P_AND12, P_AND13, P_AND14, P_AND15, P_AND16, 
-		 P_AND17, P_AND18, P_AND19, P_AND20, P_AND21, P_AND22, P_AND23, P_AND24,
-		 P_OR1, P_OR2, P_OR3, P_OR4, P_OR5, P_OR6, P_OR7: std_logic;
+SIGNAL p_and1, p_and2, p_and3, p_and4, p_and5, p_and6, p_and7, p_and8, 
+		 p_and9, p_and10, p_and11, p_and12, p_and13, p_and14, p_and15, p_and16, 
+		 p_and17, p_and18, p_and19, p_and20, p_and21, p_and22, p_and23, p_and24,
+		 p_OR1, p_OR2, p_OR3, p_OR4, p_OR5, p_OR6, p_OR7: STD_LOGIC;
 BEGIN
 
-P_AND1 <= B_di7s and (not C_di7s) and D_di7s;
-P_AND2 <= (not B_di7s) and (not D_di7s);
-P_AND3 <= (not A_di7s) and C_di7s;
-P_AND4 <= A_di7s and (not B_di7s);
-P_AND5 <= A_di7s and (not C_di7s);
-P_AND6 <= (not A_di7s) and (not C_di7s) and (not D_di7s);
-P_AND7 <= (not A_di7s) and C_di7s and D_di7s;
-P_AND8 <= A_di7s and C_di7s and (not D_di7s);
-P_AND9 <= (not A_di7s) and (not B_di7s);
-P_AND10 <= (not B_di7s) and (not C_di7s);
-P_AND11 <= (not A_di7s) and D_di7s;
-P_AND12 <= (not A_di7s) and B_di7s;
-P_AND13 <= (not A_di7s) and B_di7s and (not C_di7s) and D_di7s;
-P_AND14 <= (not A_di7s) and (not B_di7s) and (not D_di7s);
-P_AND15 <= (not A_di7s) and (not B_di7s) and C_di7s;
-P_AND16 <= (not A_di7s) and C_di7s and (not D_di7s);
-P_AND17 <= A_di7s and (not C_di7s) and (not D_di7s);
-P_AND18 <= A_di7s and C_di7s and D_di7s;
-P_AND19 <= C_di7s and (not D_di7s);
-P_AND20 <= A_di7s and C_di7s;
-P_AND21 <= A_di7s and B_di7s;
-P_AND22 <= (not C_di7s) and (not D_di7s);
-P_AND23 <= B_di7s and (not C_di7s);
-P_AND24 <= B_di7s and (not D_di7s);
+p_and1 <= B_di7s AND (NOT C_di7s) AND D_di7s;
+p_and2 <= (NOT B_di7s) AND (NOT D_di7s);
+p_and3 <= (NOT A_di7s) AND C_di7s;
+p_and4 <= A_di7s AND (NOT B_di7s);
+p_and5 <= A_di7s AND (NOT C_di7s);
+p_and6 <= (NOT A_di7s) AND (NOT C_di7s) AND (NOT D_di7s);
+p_and7 <= (NOT A_di7s) AND C_di7s AND D_di7s;
+p_and8 <= A_di7s AND C_di7s AND (NOT D_di7s);
+p_and9 <= (NOT A_di7s) AND (NOT B_di7s);
+p_and10 <= (NOT B_di7s) AND (NOT C_di7s);
+p_and11 <= (NOT A_di7s) AND D_di7s;
+p_and12 <= (NOT A_di7s) AND B_di7s;
+p_and13 <= (NOT A_di7s) AND B_di7s AND (NOT C_di7s) AND D_di7s;
+p_and14 <= (NOT A_di7s) AND (NOT B_di7s) AND (NOT D_di7s);
+p_and15 <= (NOT A_di7s) AND (NOT B_di7s) AND C_di7s;
+p_and16 <= (NOT A_di7s) AND C_di7s AND (NOT D_di7s);
+p_and17 <= A_di7s AND (NOT C_di7s) AND (NOT D_di7s);
+p_and18 <= A_di7s AND C_di7s AND D_di7s;
+p_and19 <= C_di7s AND (NOT D_di7s);
+p_and20 <= A_di7s AND C_di7s;
+p_and21 <= A_di7s AND B_di7s;
+p_and22 <= (NOT C_di7s) AND (NOT D_di7s);
+p_and23 <= B_di7s AND (NOT C_di7s);
+p_and24 <= B_di7s AND (NOT D_di7s);
 
-P_OR1 <= P_AND1 or P_AND2 or P_AND3 or P_AND4 or P_AND5;
-P_OR2 <= P_AND6 or P_AND7 or P_AND8 or P_AND9 or P_AND10;
-P_OR3 <= P_AND8 or P_AND10 or P_AND11 or P_AND12;
-P_OR4 <= P_AND13 or P_AND14 or P_AND15 or P_AND16 or P_AND17 or P_AND18;
-P_OR5 <= P_AND2 or P_AND19 or P_AND20 or P_AND21;
-P_OR6 <= P_AND22 or P_AND23 or P_AND24 or A_di7s;
-P_OR7 <= P_AND5 or P_AND15 or P_AND19 or P_AND23;
+p_OR1 <= p_and1 OR p_and2 OR p_and3 OR p_and4 OR p_and5;
+p_OR2 <= p_and6 OR p_and7 OR p_and8 OR p_and9 OR p_and10;
+p_OR3 <= p_and8 OR p_and10 OR p_and11 OR p_and12;
+p_OR4 <= p_and13 OR p_and14 OR p_and15 OR p_and16 OR p_and17 OR p_and18;
+p_OR5 <= p_and2 OR p_and19 OR p_and20 OR p_and21;
+p_OR6 <= p_and22 OR p_and23 OR p_and24 OR A_di7s;
+p_OR7 <= p_and5 OR p_and15 OR p_and19 OR p_and23;
 
-L1 <= not P_OR1;
-L2 <= not P_OR2;
-L3 <= not P_OR3;
-L4 <= not P_OR4;
-L5 <= not P_OR5;
-L6 <= not P_OR6;
-L7 <= not P_OR7;
+L1 <= NOT p_OR1;
+L2 <= NOT p_OR2;
+L3 <= NOT p_OR3;
+L4 <= NOT p_OR4;
+L5 <= NOT p_OR5;
+L6 <= NOT p_OR6;
+L7 <= NOT p_OR7;
 
 END logic;
